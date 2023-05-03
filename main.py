eb3d00ff98be51474e3f0e9918013c48b4629151
BunnaNy <12402656-BunnaNy@users.noreply.replit.com>
2023-05-03 00:54:27 +0000

    added creating a db

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
-from flask import Flask, render_template
-from database import index
+from flask import Flask, render_template, g
+import sqlite3
 
 # Create a Flask app
-app = Flask(
-    __name__,
-    template_folder='templates',
-    static_folder='static'
-)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
-#names = ["Marilyn", "Manson"]
 
-names = index()
-print(names)
+def get_db():
+    """
+    Gets database else creates it
+    """
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect("users.db")
+        # Create table in here db.execute("CREATE")
+        return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """
+    Closes connection
+    """
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    c = db.cursor()
+    c.execute("SELECT * FROM users")
+    users = c.fetchall()
+    return users
+
+
+# names = ["Marilyn", "Manson"]
+
+# names = names
+# print(names)
 
 # Index page
+"""
 @app.route('/')
 def index():
     return render_template('index.html', names=names)
+    """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run the Flask app
-    app.run(
-        host='0.0.0.0',
-        debug=True,
-        port=8080
-    )
+    app.run(host="0.0.0.0", debug=True, port=8080)
